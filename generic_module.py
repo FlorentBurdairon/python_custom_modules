32a86fd9ee233a60010580720a185228afbcea4f
Florent Burdairon <florent.burdairon@imft.fr>
2021-09-10 17:55:50 +0200

    Ajoute un module pour fonctions generiques

---
+++ generic_module.py
@@ -0,0 +1,54 @@
+# Generic modules that can be used in different situations
+
+#================================================================================================
+#================================================================================================
+
+# python librairies
+import os
+from termcolor import colored
+import inspect
+import numpy as np
+
+#================================================================================================
+#================================================================================================
+
+def print_colored_arrow_with ( section_number ):
+    print(colored("------> %u) " % section_number, "green"), end='')
+
+def check_if_string_is_an_integer( mystring ):
+    try:
+        int( mystring )
+        return True
+    except ValueError:
+        return False
+
+def extract_relative_path_from_absolute_path( directory_path ):
+    list_of_successive_directories = directory_path.split('/') # split based on the directory separator '/'
+    #print(f"{list_of_successive_directories}")
+    list_of_successive_directories = list(filter(None, list_of_successive_directories)) # remove empty strings
+    #print(f"{list_of_successive_directories}")
+    relative_path = list_of_successive_directories[-1]
+    return relative_path
+
+def get_dat_files_from_directory_and_sort_them ( mydirectory ):
+    mylist_of_files = os.listdir ( mydirectory )
+    mylist_of_dat_files = list()
+    for current_file in mylist_of_files:
+        if ".dat" in current_file:
+            if check_if_string_is_an_integer( current_file[-5] ): # there could be dat files whose names end with "scalar_field.dat" that we dont want
+                mylist_of_dat_files.append( current_file )
+#    print( mylist_of_dat_files )
+    mylist_of_dat_files.sort()
+    return mylist_of_dat_files
+
+def throw_error_with_function_name():
+    print(f"Error in [{inspect.stack()[1][3]}] from file [{inspect.stack()[1][1]}]")
+
+def convert_string_to_numpy_array(mystring):
+    nums = mystring.split()
+    ni = len(nums)
+    numpy_array = np.zeros((ni))
+    for i in range(ni):
+        numpy_array[i] = float(nums[i])
+    return numpy_array
+
