18577f3aa14b7eb2a64ebb6039067cfe9d65f33e
Florent Burdairon <florent.burdairon@imft.fr>
2022-12-15 17:36:47 +0100

    Reorganise la routine fig_init
    - rend tous les arguments d'entree optionnels avec valeurs par defaut
    - certains arguments ont pour valeur par defaut None : xlabels, ylabels,
    titles, fig_num et fig_size
    - gere l'initialisation des arguments = None par defaut
        > xlabels, ylabels, titles : alloue une liste de strings vides de la
    taille nrows*ncols ; si deja alloues, verifie que les tailles sont
    convenables, sinon exit
        > fig_num : alloue un numero non utilise (a partir de
    pyplot.get_fignums()) ; prend egalement en compte le cas ou fig_num est
    donne mais avec une valeur deja allouee => en alloue une autre
        > fig_size : alloue une taille par defaut (si None ou vide)

--- plot_module.py
+++ plot_module.py
@@ -19,25 +19,49 @@ def get_figure_dimensions(fw_pixels, fh_pixels):
     #print(figure_width, figure_height, myfigure_dpi)
     return figure_width, figure_height, myfigure_dpi
 
-def set_fig_elements_size():
+def set_fig_and_labels_size(fig_size):
     plt.rcParams['axes.labelsize'] = 25
     plt.rcParams['axes.titlesize'] = 15
     plt.rcParams['figure.titlesize'] = 20
     plt.rcParams['xtick.labelsize'] = 12
     plt.rcParams['ytick.labelsize'] = 12
-
-def fig_init(axs_rows, axs_cols, share_x_axis, share_y_axis, xlabels, ylabels, titles, suptitle_str, fig_num, fig_pos, fig_size):
-    if len(fig_size)==0:
+    #
+    if fig_size is None or len(fig_size)==0:
         fw,fh,dpi = get_figure_dimensions(1920,1080)
         #fs = min(fw,fh)
         fs = fh * 20./13.
         fig_size = [fs, fh] # [fs/1.333,fs]
-    #print(fig_size)
-    set_fig_elements_size()
-    if len(xlabels)!=axs_rows*axs_cols or len(ylabels)!=axs_rows*axs_cols or len(titles)!=axs_rows*axs_cols:
-        print("Error : mismatch in # of (xlabels/ylabels/titles)") ; exit()
+    return fig_size
+
+def allocate_label(labels, nrows, ncols):
+    if labels is None:
+        labels = [""]*nrows*ncols
+    else:
+        if len(xlabels)!=axs_rows*axs_cols or len(ylabels)!=axs_rows*axs_cols or len(titles)!=axs_rows*axs_cols:
+            print("Error : mismatch in # of (xlabels/ylabels/titles)") ; exit()
+    return labels
+
+def allocate_fig_num(fig_num):
+    fig_num_list = plt.get_fignums()
+    if fig_num is None or fig_num in fig_num_list:
+        fig_num = len(fig_num_list)
+        while fig_num in fig_num_list:
+            fig_num+=1
+    return fig_num
+
+def fig_init(axs_rows=1, axs_cols=1, share_x_axis=False, share_y_axis=False, xlabels=None, ylabels=None, titles=None, suptitle_str="", fig_num=None, fig_pos=0, fig_size=None):
+    #
+    xlabels = allocate_label(xlabels, axs_rows, axs_cols)
+    ylabels = allocate_label(ylabels, axs_rows, axs_cols)
+    titles = allocate_label(titles, axs_rows, axs_cols)
+    #
+    fig_num = allocate_fig_num(fig_num)
+    fig_size = set_fig_and_labels_size(fig_size)
+    #
     fig, axs = plt.subplots(nrows=axs_rows, ncols=axs_cols, sharex=share_x_axis, sharey=share_y_axis, num=fig_num, figsize=( fig_size[0], fig_size[1] ), clear=True, constrained_layout=True)
     fig.canvas.manager.window.move(fig_pos,0)
+    #
+    plt.suptitle(suptitle_str)
     #
     if axs_rows*axs_cols==1:
         axs.cla()
@@ -68,5 +92,4 @@ def fig_init(axs_rows, axs_cols, share_x_axis, share_y_axis, xlabels, ylabels, t
                 axs[i,j].set_xlabel(xlabels[i*axs_cols+j])
                 axs[i,j].set_ylabel(ylabels[i*axs_cols+j])
                 axs[i,j].set_title(titles[i*axs_cols+j])#, y=1.01)
-    plt.suptitle(suptitle_str)
     return fig, axs
