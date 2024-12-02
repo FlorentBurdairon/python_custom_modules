e01af6cf4641cbd6c64ad6eb4b7654decb396469
Florent Burdairon <burdairon@alastor.cerfacs.fr>
2024-12-02 15:42:05 +0100

    Ajoute test os pour methode placement des figures
    Si macos => use tkagg

--- module_plot.py
+++ module_plot.py
@@ -4,11 +4,15 @@
 #===========================================================
 
 # python librairies
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
 import os
 import tkinter
+import sys
+import numpy as np
+import matplotlib
+if sys.platform == "darwin":
+    matplotlib.use("tkagg")
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 #===========================================================
 #===========================================================
@@ -16,8 +20,8 @@ import tkinter
 
 def get_screen_dimensions():
     root = tkinter.Tk()
-    screenwidth = root.winfo_screenwidth()
-    screenheight = root.winfo_screenheight()
+    screenwidth_pixel = root.winfo_screenwidth()
+    screenheight_pixel = root.winfo_screenheight()
     pixel_per_inch = root.winfo_fpixels('1i') #dpi
     root.destroy()
     #
@@ -78,7 +82,11 @@ def fig_init(axs_rows=1, axs_cols=1, share_x_axis=False, share_y_axis=False, xla
     fig_size = set_fig_and_labels_size(fig_size)
     #
     fig, axs = plt.subplots(nrows=axs_rows, ncols=axs_cols, sharex=share_x_axis, sharey=share_y_axis, num=fig_num, figsize=( fig_size[0], fig_size[1] ), clear=True, constrained_layout=True)
-    fig.canvas.manager.window.move(fig_pos,0)
+    if sys.platform == "darwin":
+        figpos = f"+{fig_pos:d}+{0:d}"
+        fig.canvas.manager.window.wm_geometry(figpos)
+    else:
+        fig.canvas.manager.window.move(fig_pos,0)
     #
     plt.suptitle(suptitle_str)
     #
